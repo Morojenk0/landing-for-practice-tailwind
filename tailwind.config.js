5d59419c496f726deff6d88cd431650f4d7fd146
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2023-07-18 22:37:27 +0600

    started writing header code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,36 @@ module.exports = {
 	content: ["./src/index.html", "./src/components/*.{js, jsx}"],
 	theme: {
 		screens: {
-			tablet: "640px",
-			laptop: "1024px",
-			desktop: "1280px",
+			// tablet: "640px",
+			// laptop: "1024px",
+			// desktop: "1280px",
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
 		},
-		extend: {
-			colors: {},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			blue: {
+				DEFAULT: "#4452FE",
+				900: "#112D57",
+			},
+			black: {
+				DEFAULT: "#000000",
+				900: "#181818",
+			},
+			white: "#ffffff",
+			teal: "#02E4C0",
+			grey: "#5C5C5C",
+		},
+		fontFamily: {
+			graphik: "Graphik",
+			roboto: "Roboto",
+		},
+		container: {
+			center: "true",
 		},
 	},
 	variants: {
