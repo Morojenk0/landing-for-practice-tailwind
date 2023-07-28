934c1ac2671cb45a941ae83de5f3a3571fae2d32
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2023-07-28 20:59:26 +0600

    test postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,18 @@
 const tailwindcss = require("tailwindcss");
 module.exports = {
-	plugins: ["postcss-preset-env", tailwindcss],
+	plugins: [
+		require("autoprefixer"),
+		require("postcss-preset-env")({
+			stage: 1,
+		}),
+		require("precss"),
+		require("postcss-import"),
+		require("postcss-assets")({
+			loadPaths: ["dist/images"],
+		}),
+		require("cssnano")({
+			preset: "default",
+		}),
+		require("tailwindcss"),
+	],
 };
